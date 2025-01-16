2e690817a2fb8cc3c0f17acf06c03747b1ad00b1
Krozac <theo.guenezan@gmail.com>
2025-01-16 21:04:26 +0100

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import {Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import AlbumDetail from './AlbumDetail';
 
 function App() {
   return (
-    <Router>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/album/:id" element={<AlbumDetail />} />
         </Routes>
       </div>
-    </Router>
   );
 }
 
