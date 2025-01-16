03e51490865f3f93ffdd03a5072e0f2c5f38809a
Krozac <theo.guenezan@gmail.com>
2025-01-16 21:16:38 +0100

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import AlbumDetail from './AlbumDetail';
 
 function App() {
   return (
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/album/:id" element={<AlbumDetail />} />
-        </Routes>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/album/:id" element={<AlbumDetail />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-      <App />
-    </Router>
+  <Router>
+    <App />
+  </Router>
 );
